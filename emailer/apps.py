6c166702ab15e0c7ede49c8744c21c88afb39920
isaanitaya <isaanita30@gmail.com>
2022-12-29 14:29:49 +0700

    new project

--- emailer/apps.py
+++ emailer/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AuthenticationnsConfig(AppConfig):
+class EmailerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'authenticationns'
+    name = 'emailer'
